4195bac41375464f53bc41c3b63906ede6ed7123
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2026-01-13 19:12:30 +0100

    next try of plugin

--- plugin.rb
+++ plugin.rb
@@ -1,15 +1,28 @@
 # name: discourse-posthog-identify
-# about: PostHog identify endpoint for Discourse
-# version: 1.0.0
+# about: Registers a POST-endpoint for PostHog user identification
+# version: 1.0.1
 # authors: Christoph Dyllick-Brenzinger
 # url: https://github.com/christophdb/discourse-posthog-identify
 
-enabled = true
+enabled_site_setting :posthog_identify_enabled
+
 after_initialize do
-  Discourse::Application.routes.append do
-    post "/posthog/identify" => "posthog#identify"
+  module ::PosthogIdentify
+    class IdentifyController < ::ApplicationController
+      # Erzwingt Login (liefert 403, wenn nicht eingeloggt)
+      before_action :ensure_logged_in
+      
+      def identify
+        render json: {
+          id: current_user.id,
+          username: current_user.username,
+          email: current_user.email
+        }
+      end
+    end
   end
-  
-  # Optional: Admin Panel
-  add_admin_route "posthog.title", "/admin/plugins/posthog"
+
+  Discourse::Application.routes.append do
+    post "/posthog/identify" => "posthog_identify/identify#identify"
+  end
 end
